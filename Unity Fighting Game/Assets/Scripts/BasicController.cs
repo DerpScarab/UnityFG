d0bd6c832d05c3d2ddd63006ed92b3e639523a3a
Sebastian <scarabbeetlearmy@gmail.com>
2020-05-11 01:27:32 -0700

    added to the input manager

---
+++ Unity Fighting Game/Assets/Scripts/BasicController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+    InputCollecter iC = new InputCollecter();
+    float fWalk;
+    float bWalk;
+    int   airOptions;
+    float jumpHeight;
+    float gravity;
+    int   health;
+    bool  facingRight;
+    bool  isP1 = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fWalk       = .5f;
+        bWalk       = -.45f;
+        airOptions  = 1;
+        jumpHeight  = 10;
+        gravity     = .01f;
+        health      = 10000;
+        facingRight = isP1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double left;
+        double right;
+
+        if (facingRight){
+            left = bWalk;
+            right = fWalk;
+        } else {
+            right = bWalk;
+            left = fWalk;
+        }
+
+        if (iC.fInput == 6){
+            transform.translate();
+        }
+        if (iC.fInput == 4){
+            transform.translate();
+        }
+    }
+}
---
+++ Unity Fighting Game/Assets/Scripts/InputCollector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCollector : MonoBehaviour
+{
+    public ArrayList<Integer> inputStream = new ArrayList();
+    public int xInput;
+    public int yInput;
+    public int fInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xInput = 0;
+        yInput = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+        if (!facingRight){xInput = -xInput;}
+
+        if (xInput == -1 && yInput == -1){fInput = 1;}
+        if (xInput == 0 && yInput == -1) {fInput = 2;}
+        if (xInput == 1 && yInput == -1) {fInput = 3;}
+        if (xInput == -1 && yInput == 0) {fInput = 4;}
+        if (xInput == 0 && yInput == 0)  {fInput = 5;}
+        if (xInput == 1 && yInput == 0)  {fInput = 6;}
+        if (xInput == -1 && yInput == 1) {fInput = 7;}
+        if (xInput == 0 && yInput == 1)  {fInput = 8;}
+        if (xInput == 1 && yInput == 1)  {fInput = 9;}
+
+        inputStream.add(fInput);
+    }
+}
